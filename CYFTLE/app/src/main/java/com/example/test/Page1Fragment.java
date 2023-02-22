45b6aa253f9466a8077bb9479c5fa62bdac8bff9
TA-PP <kgrewd@naver.com>
2023-02-22 16:25:42 +0900

    필요 없는 파일 버리고 디렉토리 이름 변경

---
+++ CYFTLE/app/src/main/java/com/example/test/Page1Fragment.java
@@ -0,0 +1,59 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+
+
+public class Page1Fragment extends Fragment {
+
+    private String responseString;
+    private TextView duck;
+    private SharedViewModel sharedViewModel;
+
+
+    // stt로 가져온 데이터 서버로 보내기
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+        sharedViewModel.getLiveData().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                duck.setText(s);
+            }
+        });
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_page1, container, false);
+
+        sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+
+        ImageView duck_image = (ImageView) view.findViewById(R.id.ducktale_image);
+
+        duck = (TextView) view.findViewById(R.id.duck_tale);
+        // textview 스크롤 가능하게 한다
+        duck.setMovementMethod(new ScrollingMovementMethod());
+
+
+        return view;
+    }
+}
